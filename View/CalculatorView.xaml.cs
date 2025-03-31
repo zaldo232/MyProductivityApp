9677039fefa2f68caa2ce76e2c9f414e461c76d4
zaldo232 <zaldo232@naver.com>
2025-04-01 03:00:49 +0900

    Calculator 완성

---
+++ View/CalculatorView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows.Controls;
+using System.Windows.Input;
+using MyProductivityApp.ViewModel;
+
+namespace MyProductivityApp.View
+{
+    public partial class CalculatorView : UserControl
+    {
+        public CalculatorView()
+        {
+            InitializeComponent();
+            this.DataContext = new CalculatorViewModel();
+        }
+
+        private void HistoryListBox_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (sender is ListBox listBox && listBox.SelectedItem is string history)
+            {
+                // "식 = 결과" → 식 부분만 추출
+                var expression = history.Split('=')[0].Trim();
+                ((CalculatorViewModel)this.DataContext).Input = expression;
+            }
+        }
+
+    }
+
+}
---
+++ ViewModel/CalculatorViewModel.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace MyProductivityApp.ViewModel
+{
+    public class CalculatorViewModel : INotifyPropertyChanged
+    {
+        private string input = "";
+
+        public string Input
+        {
+            get => input;
+            set
+            {
+                input = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand AppendCommand { get; }
+        public ICommand ClearCommand { get; }
+        public ICommand CalculateCommand { get; }
+        public ObservableCollection<string> History { get; set; } = new();
+
+        public CalculatorViewModel()
+        {
+            AppendCommand = new RelayCommand<string>(Append);
+            ClearCommand = new RelayCommand(Clear);
+            CalculateCommand = new RelayCommand(Calculate);
+        }
+
+        private void Append(string value)
+        {
+            Input += value;
+        }
+
+        private void Clear()
+        {
+            Input = "";
+        }
+
+        private void Calculate()
+        {
+            try
+            {
+                var result = new System.Data.DataTable().Compute(Input, null);
+                History.Insert(0, $"{Input} = {result}"); // 최신 항목 위에
+                Input = result.ToString();
+            }
+            catch
+            {
+                Input = "오류";
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] string name = null)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
